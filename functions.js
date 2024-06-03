d708ce2ff64719527827b4dbe51be498cbc30f4b
Tennelle SWAN <105378467+TechTens@users.noreply.github.com>
2024-06-02 18:01:29 -0600

    Add files via upload
    Added root HTML files, CSS file, and Javascript file

---
+++ functions.js
@@ -0,0 +1,59 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  //overlay handler//
+  function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
+
+  //map api//
+  function myMap() {
+    var mapProp= {
+      center:new google.maps.LatLng(39.753810,-104.923720),
+      zoom:15,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(39.753810, -104.923720
+      ),
+      map: map,
+   }); 
+    }
+
+    //slideshow//
+    let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
